c3ab81d84e92e4254f777033b95b279f202eea9e
Rafael Persegani <rafaper19@gmail.com>
2020-02-07 15:15:24 -0800

    Fixed bug to allowing values to be added to variables outside function. Deleted old files and moved another.

--- PowerShell_Az_AddorDeleteNICs/AZ_Add+Delete_NICs/Az_Add+Delete_NICs.ps1
+++ PowerShell_Az_AddorDeleteNICs/AZ_Add+Delete_NICs/Az_Add+Delete_NICs.ps1
@@ -26,7 +26,7 @@ function ManageNetworkInterfaces {
 		        $newNICnsgId = (Get-AzNetworkSecurityGroup -ResourceGroupName $myRG -Name $newNICnsg).Id
 		        $newNIC = New-AzNetworkInterface -Name $newNICinput -ResourceGroupName $myRG -Location $newNICLocation -SubnetId $mySubBackendObj.Id -NetworkSecurityGroupId $newNICnsgId
                 $newNicId = (Get-AzNetworkInterface -ResourceGroupName $myRG -Name $newNICinput).Id
-		        $myVmObj = Add-AzVMNetworkInterface -VM $myVmObj -Id $newNicId -Primary
+		        $script:myVmObj = Add-AzVMNetworkInterface -VM $myVmObj -Id $newNicId -Primary
                 echo "Updating Virtual Machine."
 		        Update-AzVm -ResourceGroupName $myRG -VM $myVmObj
                 Write-Host "Action Complete." -ForegroundColor Green
@@ -47,19 +47,19 @@ function ManageNetworkInterfaces {
 
 function LoadConfigFile {
     echo "Please enter the full file path without quotes:"
-    $configFilePath = Read-Host.Replace('"',"")
+    $configFilePath = Read-Host
 
     echo "Loading config file inputs..."
     $configContents = Get-Content $configFilePath.Replace('"',"")
 
-    $myRG = $configContents[0].split("=")[1].Trim(" ")
-    $myVm = $configContents[1].split("=")[1].Trim(" ")
-    $myVnet = $configContents[2].split("=")[1].Trim(" ")
+    $script:myRG = $configContents[0].split("=")[1].Trim(" ")
+    $script:myVm = $configContents[1].split("=")[1].Trim(" ")
+    $script:myVnet = $configContents[2].split("=")[1].Trim(" ")
     $mySubBackend = $configContents[3].split("=")[1].Trim(" ")
 
-    $myVmObj = Get-AzVM -ResourceGroupName $myRG -Name $myVm
-    $myVnetObj = Get-AzVirtualNetwork -Name $myVnet -ResourceGroupName $myRG
-    $mySubBackendObj = $myVnetObj.Subnets|?{$_.Name -eq $mySubBackend}
+    $script:myVmObj = Get-AzVM -ResourceGroupName $myRG -Name $myVm
+    $script:myVnetObj = Get-AzVirtualNetwork -Name $myVnet -ResourceGroupName $myRG
+    $script:mySubBackendObj = $myVnetObj.Subnets|?{$_.Name -eq $mySubBackend}
     echo "Loading complete."
 }
 
@@ -68,24 +68,24 @@ function RunWithoutConfigFile {
     echo "Loading Resource Groups..."
     Get-AzResourceGroup
     echo "Enter Resource Group Name to list existing VMs:"
-    $myRG = Read-Host
+    $script:myRG = Read-Host
 
     echo "Loading Virtual Machines..."
     Get-AzVM -ResourceGroupName $myRG
     echo "Enter name of VM you would like to edit?"
     echo "Please note adding/removing NICs will require VM to be shut down."
-    $myVm = Read-Host
-    $myVmObj = Get-AzVM -ResourceGroupName $myRG -Name $myVm
+    $script:myVm = Read-Host
+    $script:myVmObj = Get-AzVM -ResourceGroupName $myRG -Name $myVm
 
     Stop-AzVM -ResourceGroupName $myRG -Name $myVm
 
     echo "Enter the name of the Virtual Network associated with the VM:"
-    $myVnet = Read-Host
-    $myVnetObj = Get-AzVirtualNetwork -Name $myVnet -ResourceGroupName $myRG
+    $script:myVnet = Read-Host
+    $script:myVnetObj = Get-AzVirtualNetwork -Name $myVnet -ResourceGroupName $myRG
 
     echo "Enter the name of the Subnet Backend associated with the VM:" 
-    $mySubBackend = Read-Host
-    $mySubBackendObj = $myVnetObj.Subnets|?{$_.Name -eq $mySubBackend}
+    $script:mySubBackend = Read-Host
+    $script:mySubBackendObj = $myVnetObj.Subnets|?{$_.Name -eq $mySubBackend}
 }
 
 Connect-AzAccount
